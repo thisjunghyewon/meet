19c79c915514faf5fd272554719b4b2a54bab061
thisjunghyewon <this.junghyewon@gmail.com>
2023-09-20 15:28:22 +0200

    Write code for serverless function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,35 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
+const { google } = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events.public.readonly",
+];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ["https://thisjunghyewon.github.io/meet/"];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+//This instance accepts CLIENT_ID, CLIENT_SECRET, and the redirect URL as parameters.
+
+module.exports.getAuthURL = async () => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  // which allows you to seamlessly retrieve an access token, refresh it, and retry the request.
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
